27ede99f4e8dce882a92358b77b8f6690fdd279e
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-04-26 14:47:25 +0700

    fix: enable threading capability

--- build.zig
+++ build.zig
@@ -49,6 +49,9 @@ pub fn build(b: *std.Build) void {
     });
     b.installArtifact(sharedLib);
 
+    // need libc for threading capability
+    sharedLib.linkLibC();
+
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
