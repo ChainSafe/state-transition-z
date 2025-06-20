a193fb095cab943e2a26d9e57f25e7f4c5ee9a52
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-06-20 09:44:41 +0700

    feat: implement EffectiveBalanceIncrements

---
+++ src/cache/effective_balance_increments.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ssz = @import("consensus_types");
+const preset = ssz.preset;
+pub const EffectiveBalanceIncrements = std.ArrayList(u16);
+const BeaconStateAllForks = @import("../beacon_state.zig").BeaconStateAllForks;
+const EFFECTIVE_BALANCE_INCREMENT = @import("../params.zig").EFFECTIVE_BALANCE_INCREMENT;
+
+pub fn getEffectiveBalanceIncrementsZeroed(allocator: Allocator, len: usize) !EffectiveBalanceIncrements {
+    var increments = EffectiveBalanceIncrements.init(allocator);
+    try increments.ensureTotalCapacity(len);
+    for (0..len) |_| {
+        try increments.append(0);
+    }
+    return increments;
+}
+
+pub fn getEffectiveBalanceIncrementsWithLen(allocator: Allocator, validator_count: usize) !EffectiveBalanceIncrements {
+    const len = 1024 * @divFloor(validator_count, 1024);
+    return getEffectiveBalanceIncrementsZeroed(allocator, len);
+}
+
+pub fn getEffectiveBalanceIncrements(allocator: Allocator, state: BeaconStateAllForks) !EffectiveBalanceIncrements {
+    var increments = EffectiveBalanceIncrements.init(allocator);
+    const validator_count = state.getValidatorsCount();
+    increments.ensureTotalCapacity(validator_count);
+
+    for (0..validator_count) |i| {
+        const validator = state.getValidator(i);
+        try increments.append(@divFloor(validator.effective_balance, preset.EFFECTIVE_BALANCE_INCREMENT));
+    }
+}
+
+// TODO: unit tests
