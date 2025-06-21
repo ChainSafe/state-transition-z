41931b20e42e95ba49580af19557331dc5333ac5
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-06-21 10:44:56 +0700

    feat: implement sync_committee utils

--- src/utils/seed.zig
+++ src/utils/seed.zig
@@ -8,14 +8,17 @@ const Epoch = ssz.primitive.Epoch.Type;
 const Bytes32 = ssz.primitive.Bytes32.Type;
 const DomainType = ssz.primitive.DomainType.Type;
 const ForkSeq = @import("../config.zig").ForkSeq;
+const params = @import("../params.zig");
 const EPOCHS_PER_HISTORICAL_VECTOR = ssz.preset.EPOCHS_PER_HISTORICAL_VECTOR;
 const MIN_SEED_LOOKAHEAD = ssz.preset.MIN_SEED_LOOKAHEAD;
 const ValidatorIndices = @import("../type.zig").ValidatorIndices;
 const EffiectiveBalanceIncrements = @import("../cache/effective_balance_increments.zig").EffectiveBalanceIncrements;
 const computeStartSlotAtEpoch = @import("./epoch.zig").computeStartSlotAtEpoch;
 const computeProposerIndex = @import("./committee_indices.zig").computeProposerIndex;
+const computeSyncCommitteeIndices = @import("./committee_indices.zig").computeSyncCommitteeIndices;
+const computeEpochAtSlot = @import("./epoch.zig").computeEpochAtSlot;
 
-pub fn computeProposers(allocator: Allocator, fork: ForkSeq, epoch_seed: [32]u8, epoch: Epoch, active_indices: ValidatorIndices, effective_balance_increments: EffiectiveBalanceIncrements, out: [preset.SLOTS_PER_EPOCH]u32) !void {
+pub fn computeProposers(allocator: Allocator, fork: ForkSeq, epoch_seed: [32]u8, epoch: Epoch, active_indices: ValidatorIndices, effective_balance_increments: EffiectiveBalanceIncrements, out: []u32) !void {
     const start_slot = computeStartSlotAtEpoch(epoch);
     for (start_slot..start_slot + preset.SLOTS_PER_EPOCH, 0..) |slot, i| {
         const slot_buf: [8]u8 = undefined;
@@ -32,6 +35,16 @@ pub fn computeProposers(allocator: Allocator, fork: ForkSeq, epoch_seed: [32]u8,
         // TODO: generalize computeProposerIndex to handle both u32 and u64
         out[i] = try computeProposerIndex(allocator, seed, active_indices, effective_balance_increments.items, rand_byte_count, max_effective_balance, preset.EFFECTIVE_BALANCE_INCREMENT, preset.SHUFFLE_ROUND_COUNT);
     }
+}
+
+pub fn getNextSyncCommitteeIndices(allocator: Allocator, fork: ForkSeq, state: *const BeaconStateAllForks, active_indices: ValidatorIndices, effective_balance_increments: EffiectiveBalanceIncrements, out: []u32) !void {
+    const rand_byte_count = if (fork >= ForkSeq.electra) 2 else 1;
+    const max_effective_balance = if (fork >= ForkSeq.electra) preset.MAX_EFFECTIVE_BALANCE_PRE_ELECTRA else preset.MAX_EFFECTIVE_BALANCE;
+
+    const epoch = computeEpochAtSlot(state.getSlot() + 1);
+    var seed: [32]u8 = undefined;
+    try getSeed(state, epoch, params.DOMAIN_SYNC_COMMITTEE, &seed);
+    try computeSyncCommitteeIndices(allocator, seed, active_indices, effective_balance_increments.items, rand_byte_count, max_effective_balance, preset.EFFECTIVE_BALANCE_INCREMENT, preset.SHUFFLE_ROUND_COUNT, out);
 }
 
 pub fn getRandaoMix(state: BeaconStateAllForks, epoch: Epoch) Bytes32 {
---
+++ src/utils/sync_committee.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+const blst = @import("blst_min_pk");
+const BlstPublicKey = blst.PublicKey;
+const AggregatePublicKey = blst.AggregatePublicKey;
+const Allocator = std.mem.Allocator;
+const BeaconStateAllForks = @import("../beacon_state.zig").BeaconStateAllForks;
+const ValidatorIndices = @import("../type.zig").ValidatorIndices;
+const EffiectiveBalanceIncrements = @import("../cache/effective_balance_increments.zig").EffectiveBalanceIncrements;
+const ssz = @import("consensus_types");
+const preset = ssz.preset;
+const params = @import("../params.zig");
+const SyncCommittee = ssz.altair.SyncCommittee.Type;
+const PublicKey = ssz.primitive.BLSPubkey.Type;
+const getNextSyncCommitteeIndices = @import("./seed.zig").getNextSyncCommitteeIndices;
+const ForkSeq = @import("../config.zig").ForkSeq;
+
+const SyncCommitteeInfo = struct {
+    indices: ValidatorIndices,
+    sync_committee: *SyncCommittee,
+};
+
+/// Consumer must deallocate the returned `SyncCommitteeInfo` struct
+pub fn getNextSyncCommittee(allocator: Allocator, fork: ForkSeq, state: *const BeaconStateAllForks, active_validators_indices: ValidatorIndices, effecitve_balance_increment: EffiectiveBalanceIncrements) !*SyncCommitteeInfo {
+    const indices = ValidatorIndices.init(allocator).resize(preset.SYNC_COMMITTEE_SIZE);
+    try getNextSyncCommitteeIndices(allocator, fork, state, active_validators_indices, effecitve_balance_increment, indices.items);
+
+    // Using the index2pubkey cache is slower because it needs the serialized pubkey.
+    var pubkeys: [preset.SYNC_COMMITTEE_SIZE]PublicKey = undefined;
+    for (indices, 0..) |index, i| {
+        pubkeys[i] = state.getValidator(index).pubkey;
+    }
+
+    const aggregated_pk = try AggregatePublicKey.aggregateSerialized(pubkeys[0..], false);
+    const sync_committee = try allocator.create(SyncCommittee);
+    sync_committee.* = .{
+        .pubkeys = pubkeys,
+        .aggregate_pubkey = aggregated_pk,
+    };
+    return .{
+        .indices = indices,
+        .sync_committee = sync_committee,
+    };
+}
+
+pub fn computeSyncParticipantReward(total_active_balance_increments: u64) u64 {
+    const total_active_balance = total_active_balance_increments * preset.EFFECTIVE_BALANCE_INCREMENT;
+    const base_reward_per_increment = @divFloor((preset.EFFECTIVE_BALANCE_INCREMENT * preset.BASE_REWARD_FACTOR), total_active_balance);
+    const total_base_rewards = base_reward_per_increment * total_active_balance_increments;
+    const max_participant_rewards = @divFloor(@divFloor(total_base_rewards * params.SYNC_REWARD_WEIGHT, params.WEIGHT_DENOMINATOR), preset.SLOTS_PER_EPOCH);
+    return @divFloor(max_participant_rewards, preset.SYNC_COMMITTEE_SIZE);
+}
+
+pub fn computeBaseRewardPerIncrement(total_active_stake_by_increment: u64) u64 {
+    return @divFloor((preset.EFFECTIVE_BALANCE_INCREMENT * preset.BASE_REWARD_FACTOR), @sqrt(total_active_stake_by_increment * preset.EFFECTIVE_BALANCE_INCREMENT));
+}
