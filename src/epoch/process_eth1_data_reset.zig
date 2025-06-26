b03fce713d39c5a4af3a3b552f17d22d147f4bb2
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-06-26 09:21:58 +0700

    feat: processEth1DataReset

--- src/epoch/process_eth1_data_reset.zig
+++ src/epoch/process_eth1_data_reset.zig
@@ -1,7 +1,16 @@
 const ssz = @import("consensus_types");
-const phase0 = ssz.phase0;
-const BeaconState = phase0.BeaconState;
+const CachedBeaconStateAllForks = @import("../cache/state_cache.zig").CachedBeaconStateAllForks;
+const EpochTransitionCache = @import("../cache/epoch_transition_cache.zig").EpochTransitionCache;
+const preset = ssz.preset;
+const EPOCHS_PER_ETH1_VOTING_PERIOD = preset.EPOCHS_PER_ETH1_VOTING_PERIOD;
 
-pub fn processEth1DataReset(state: *BeaconState.Type) void {
-    state.eth1_data_votes = ssz.phase0.Eth1DataVotes.default_value;
+/// Reset eth1DataVotes tree every `EPOCHS_PER_ETH1_VOTING_PERIOD`.
+pub fn processEth1DataReset(cached_state: CachedBeaconStateAllForks, cache: EpochTransitionCache) void {
+    const next_epoch = cache.current_epoch + 1;
+
+    // reset eth1 data votes
+    if (next_epoch % EPOCHS_PER_ETH1_VOTING_PERIOD == 0) {
+        const state = cached_state.state;
+        state.setEth1DataVotes(ssz.phase0.Eth1DataVotes.default_value);
+    }
 }
