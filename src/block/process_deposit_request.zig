2934d4443e5ffeb615a7dc57dbcfa4e2cd4d7e82
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-07-10 19:57:11 +0700

    feat: process_deposit_request

---
+++ src/block/process_deposit_request.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const CachedBeaconStateAllForks = @import("../cache/state_cache.zig").CachedBeaconStateAllForks;
+const ssz = @import("consensus_types");
+const DepositRequest = ssz.electra.DepositRequest.Type;
+const PendingDeposit = ssz.electra.PendingDeposit.Type;
+const Root = ssz.primitive.Root.Type;
+const params = @import("../params.zig");
+
+pub fn processDepositRequest(cached_state: *CachedBeaconStateAllForks, deposit_request: *const DepositRequest) !void {
+    const state = cached_state.state;
+    if (state.getDepositRequestsStartIndex() == params.UNSET_DEPOSIT_REQUESTS_START_INDEX) {
+        state.setDepositRequestsStartIndex(deposit_request.index);
+    }
+
+    // Create pending deposit
+    const pending_deposit = PendingDeposit{
+        .pubkey = deposit_request.pubkey,
+        .withdrawal_credentials = deposit_request.withdrawal_credentials,
+        .amount = deposit_request.amount,
+        .signature = deposit_request.signature,
+        .slot = state.getSlot(),
+    };
+
+    try state.addPendingDeposit(&pending_deposit);
+}
