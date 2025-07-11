8086d778c8006e7cd9cef962ed08dab61c0fe5c5
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-07-11 14:55:19 +0700

    feat: process_slot

---
+++ src/slot/process_slot.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const CachedBeaconStateAllForks = @import("../cache/state_cache.zig").CachedBeaconStateAllForks;
+const ssz = @import("consensus_types");
+const preset = ssz.preset;
+const Root = ssz.primitive.Root.Type;
+const ZERO_HASH = @import("../constants.zig").ZERO_HASH;
+
+pub fn processSlot(allocator: Allocator, cached_state: *CachedBeaconStateAllForks) void {
+    const state = cached_state.state;
+
+    // Cache state root
+    var previous_state_root: Root = undefined;
+    try state.hashTreeRoot(allocator, &previous_state_root);
+    state.setStateRoot(state.getSlot() % preset.SLOTS_PER_HISTORICAL_ROOT, previous_state_root);
+
+    // Cache latest block header state root
+    var latest_block_header = state.getLatestBlockHeader();
+    if (!std.mem.allEqual(u8, &latest_block_header.state_root, &ZERO_HASH)) {
+        latest_block_header.state_root = previous_state_root;
+    }
+
+    // Cache block root
+    var previous_block_root: Root = undefined;
+    try ssz.phase0.BeaconBlockHeader.hashTreeRoot(latest_block_header, &previous_block_root);
+    state.setBlockRoot(state.getSlot() % preset.SLOTS_PER_HISTORICAL_ROOT, previous_block_root);
+}
