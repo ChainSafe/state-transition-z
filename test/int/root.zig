8f63229cb3344297550fe226fde99c05e57250ad
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-07-28 17:04:25 +0700

    chore: test processInactivityUpdates()

--- test/int/root.zig
+++ test/int/root.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 const testing = std.testing;
 const epoch_transition_cache = @import("./cache/epoch_transition_cache.zig");
 const process_justification_and_finalization = @import("./epoch/process_justification_and_finalization.zig");
+const process_inactivity_updates = @import("./epoch/process_inactivity_updates.zig");
 const process_rewards_and_penalties = @import("./epoch/process_rewards_and_penalties.zig");
 
 test {
     testing.refAllDecls(epoch_transition_cache);
     testing.refAllDecls(process_justification_and_finalization);
     testing.refAllDecls(process_rewards_and_penalties);
+    testing.refAllDecls(process_inactivity_updates);
 }
