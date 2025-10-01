838415002bc49429ba3d04edf973077030bfaa85
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-10-01 13:28:36 +0700

    fix: use @tagName() in Preset

--- src/params/preset.zig
+++ src/params/preset.zig
@@ -1,17 +1,9 @@
-const Preset_Mainnet = "mainnet";
-const Preset_Minimal = "minimal";
-const Preset_Gnosis = "gnosis";
-
 pub const Preset = enum(u8) {
     mainnet = 0,
     minimal = 1,
     gnosis = 2,
 
     pub fn getPresetName(self: Preset) []const u8 {
-        return switch (self) {
-            .mainnet => Preset_Mainnet,
-            .minimal => Preset_Minimal,
-            .gnosis => Preset_Gnosis,
-        };
+        @tagName(self);
     }
 };
