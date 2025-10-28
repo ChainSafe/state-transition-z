842c886d5a571916f0b62fbc193eaec0e87d54ea
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-10-28 14:55:06 +0700

    fix: processEffectiveBalanceUpdates integer underflow

--- src/state_transition/epoch/process_effective_balance_updates.zig
+++ src/state_transition/epoch/process_effective_balance_updates.zig
@@ -63,19 +63,18 @@ pub fn processEffectiveBalanceUpdates(cached_state: *CachedBeaconStateAllForks, 
             // TODO: describe issue. Compute progressive target balances
             // Must update target balances for consistency, see comments below
             if (state.isPostAltair()) {
-                const delta_effective_balance_increment = new_effective_balance_increment - effective_balance_increment;
                 const previous_epoch_participation = state.previousEpochParticipations().items;
                 const current_epoch_participation = state.currentEpochParticipations().items;
 
                 if (!validator.slashed) {
                     if (previous_epoch_participation[i] & TIMELY_TARGET == TIMELY_TARGET) {
-                        epoch_cache.previous_target_unslashed_balance_increments += delta_effective_balance_increment;
+                        epoch_cache.previous_target_unslashed_balance_increments += new_effective_balance_increment - effective_balance_increment;
                     }
 
                     // currentTargetUnslashedBalanceIncrements is transfered to previousTargetUnslashedBalanceIncrements in afterEpochTransitionCache
                     // at epoch transition of next epoch (in EpochTransitionCache), prevTargetUnslStake is calculated based on newEffectiveBalanceIncrement
                     if (current_epoch_participation[i] & TIMELY_TARGET == TIMELY_TARGET) {
-                        epoch_cache.current_target_unslashed_balance_increments += delta_effective_balance_increment;
+                        epoch_cache.current_target_unslashed_balance_increments += new_effective_balance_increment - effective_balance_increment;
                     }
                 }
             }
